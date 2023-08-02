6bf93b1b1b7fdaad670defed04a669ec5b12f87d
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-08-02 06:19:29 +0300

    feat(): improve canvas resolution

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -5,8 +5,10 @@ const socket = io();
 
 const scoreEl = document.querySelector('#scoreEl');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+const devicePixelRatio = window.devicePixelRatio ||  1
+
+canvas.width = innerWidth * devicePixelRatio;
+canvas.height = innerHeight * devicePixelRatio;
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
